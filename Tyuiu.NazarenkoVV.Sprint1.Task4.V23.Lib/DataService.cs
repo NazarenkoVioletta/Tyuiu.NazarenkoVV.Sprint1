e039904deb734b9236fc4d31aa4e34f1bc514bb1
nazarenkovv <vitanazarenko2006@yandex.ru>
2024-10-31 20:55:02 +0500

    Task 4

--- Tyuiu.NazarenkoVV.Sprint1.Task4.V23.Lib/DataService.cs
+++ Tyuiu.NazarenkoVV.Sprint1.Task4.V23.Lib/DataService.cs
@@ -15,6 +15,7 @@ namespace Tyuiu.NazarenkoVV.Sprint1.Task4.V23.Lib
         public double Calculate(double x, double y)
         {
             var res = Math.Sqrt(Math.Abs(x + y)) / Math.Abs(3 - x);
+            Math.Round(0.16643566632465154, 3);
             return res;
         }
     }
